8dd12f597490dd6a8299ff78ed1331574a2a079b
Nicolas Wanner <nicolas.th.wanner@gmail.com>
2026-09-03 08:15:16 +0200

    gen

--- java/oop-basics/src/main/java/ch/nw1n/generics/Genericum.java
+++ java/oop-basics/src/main/java/ch/nw1n/generics/Genericum.java
@@ -3,10 +3,30 @@ package ch.nw1n.generics;
 import java.util.List;
 import java.util.stream.Stream;
 
-public class Genericum {
-    public Genericum() {
+public class Genericum<T> {
+    T value;
 
+    public Genericum(T item) {
+        this.value = item;
     }
+
+    public T getValue() {
+        return this.value;
+    }
+
+    /**********************************************
+     * Static Factory
+     **********************************************/
+
+    public static void demoObjectCreation() {
+        var res = new Genericum<>("hello");
+        System.out.println(res.getValue());
+    }
+
+
+    /**********************************************
+     * Static Functions
+     **********************************************/
 
     public static void demoWrapInList() {
         var myStr = "hello";
